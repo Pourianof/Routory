ccb17e57d1a9bce925f08168f12478eb1e32b7d6
Pooria Amini <pouriyaxamini@gmail.com>
2025-01-03 03:24:37 +0330

    change the default data providing to raw data instead of json

--- src/routerRespond.ts
+++ src/routerRespond.ts
@@ -29,7 +29,7 @@ export default class RouterRespond extends Notifier<'onrespond'> {
     this.statusCode = statusCode;
     return this;
   }
-  private jsonOutput = true;
+  private jsonOutput = false;
 
   json(data: any) {
     this.jsonOutput = true;
