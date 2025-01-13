54a9e234c9e07bd4c6cf8eb24958d32b527372fa
Pooria Amini <pouriyaxamini@gmail.com>
2025-01-13 17:04:19 +0330

    add build config for code generator script

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,9 @@ const path = require('path');
 
 const isDev = !!process.argv.find((e) => e === '--config-dev');
 const destRootDir = path.join('dist', 'routory');
+const distBinDir = path.join(destRootDir, 'bin');
 
+/** @type {import('rollup').RollupOptions} */
 const config = [
   {
     input: 'src/routory.ts',
@@ -30,6 +32,38 @@ const config = [
           {
             src: 'Readme.md',
             dest: destRootDir,
+          },
+        ],
+      }),
+    ],
+  },
+  {
+    input: 'src/bin/index.ts',
+    output: {
+      file: path.join(distBinDir, 'index.js'),
+      format: 'cjs',
+      sourcemap: isDev,
+    },
+    plugins: [
+      typescript({
+        compilerOptions: {
+          target: 'es2016',
+          module: 'nodenext',
+          moduleResolution: 'nodenext',
+          declaration: false,
+          declarationDir: distBinDir,
+        },
+      }),
+      copy({
+        targets: [
+          { src: 'src/bin/class_template.txt', dest: distBinDir },
+          {
+            src: 'src/bin/example_http_routory.json',
+            dest: distBinDir,
+          },
+          {
+            src: 'src/bin/method_template.txt',
+            dest: distBinDir,
           },
         ],
       }),
